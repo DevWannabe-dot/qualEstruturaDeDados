e5cfce17f839904c591a6f687db377de813eaef3
Pedro Henrique <spyxtreme41@gmail.com>
2023-04-21 13:48:30 -0300

    Add 'pilhas' library

---
+++ pilhas.h
@@ -0,0 +1,35 @@
+#pragma once
+#ifndef __PILHAS_h__
+#define __PILHAS_h__
+
+#define N_PILHAS 500
+
+typedef struct pilha_vet {
+	int n;
+	char vet[N_PILHAS];
+} Pilha_vet;
+
+typedef struct lista_p{
+	char info;
+	struct lista_p *prox;
+} Lista_p;
+
+typedef struct pilha_lst{
+	Lista_p *prim;
+} Pilha_lst;
+
+Pilha_vet *pilha_vet_cria();
+void pilha_vet_push(Pilha_vet *p, char v);
+char pilha_vet_pop(Pilha_vet *p);
+int pilha_vet_vazia(Pilha_vet *p);
+void pilha_vet_libera(Pilha_vet *p);
+void pilha_vet_imprime(Pilha_vet *p);
+
+Pilha_lst *pilha_lst_cria();
+void pilha_lst_push(Pilha_lst *p, char v);
+char pilha_lst_pop(Pilha_lst *p);
+int pilha_lst_vazia(Pilha_lst *p);
+void pilha_lst_libera(Pilha_lst *p);
+void pilha_lst_imprime(Pilha_lst *p);
+
+#endif
